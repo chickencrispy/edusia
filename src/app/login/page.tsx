f56be86649878358d376d6c7008b5d15763db38c
chickencrispy <debi.rahmadi@gmail.com>
2025-01-28 11:44:09 +0700

    Refactor LoginPage component for improved layout and styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,34 +29,39 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      {error && <p className="error">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            className="p-3 border"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            className="p-3 border"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+    <div className="login-container h-dvh place-content-center">
+      <div className='w-full sm:w-1/4 mx-auto rounded-lg p-6'>
+        <h1 className='text-2xl font-bold mb-4'>Login</h1>
+
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSubmit} className='space-y-2'>
+          <div className="flex items-center">
+            <label htmlFor="email" className='w-1/3'>Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              className="input w-2/3"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex items-center">
+            <label htmlFor="password" className='w-1/3'>Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              className="input w-2/3"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className='text-end'>
+            <button type="submit" className="btn btn-accent text-sm">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
